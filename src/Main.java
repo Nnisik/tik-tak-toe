5737c479dec1f2eb08f6d2dd38ab3dc9bee1a3a8
Nina Samykova <nisik0120@gmail.com>
2024-11-03 22:14:38 +0100

    stage 2 - complite

--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,31 @@ public class Main {
         String input = scanner.nextLine();
         scanner.close();
 
+        drawHorizontalBoardline();
+
         int check = 0;
+
         for (double i = 0; i < input.length(); i++) {
-            System.out.printf("%c ", input.charAt((int)i));
+            if (check == 0) {
+                drawVerticalBoardLine();
+            }
+            System.out.print(input.charAt((int)i) + " ");
             check += 1;
             if (check == 3) {
+                drawVerticalBoardLine();
                 System.out.print("\n");
                 check = 0;
             }
         }
+
+        drawHorizontalBoardline();
+    }
+
+    static void drawHorizontalBoardline() {
+        System.out.println("---------");
+    }
+
+    static void drawVerticalBoardLine() {
+        System.out.print("| ");
     }
 }
