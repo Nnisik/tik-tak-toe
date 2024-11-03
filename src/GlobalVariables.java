0898bf78733d32a5fe9ee48e621ab9ce4dfae571
Nina Samykova <nisik0120@gmail.com>
2024-11-03 22:43:45 +0100

    stage 3 - checking current game state

---
+++ src/GlobalVariables.java
@@ -0,0 +1,3 @@
+public class GlobalVariables {
+    public static String gameResult;
+}
--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,35 @@
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
+        // String input = scanner.nextLine();
+        String input = "XO_XO_XOX";
         scanner.close();
+
+        int check = 0;
+        GlobalVariables.gameResult = "";
 
         drawHorizontalBoardline();
 
-        int check = 0;
-
         for (double i = 0; i < input.length(); i++) {
+
             if (check == 0) {
                 drawVerticalBoardLine();
             }
-            System.out.print(input.charAt((int)i) + " ");
+            drawNextElement(input.charAt((int)i));
             check += 1;
+
             if (check == 3) {
                 drawVerticalBoardLine();
                 System.out.print("\n");
                 check = 0;
             }
         }
-
         drawHorizontalBoardline();
+        System.out.println(GlobalVariables.gameResult);
     }
 
     static void drawHorizontalBoardline() {
@@ -33,5 +38,15 @@ public class Main {
 
     static void drawVerticalBoardLine() {
         System.out.print("| ");
+    }
+
+    static void drawNextElement(char symbol) {
+        if (Character.compare(symbol, '_') == 0){
+            System.out.print(" ");
+            GlobalVariables.gameResult = "Game not finished";
+        } else {
+            System.out.print(symbol);
+        }
+        System.out.print(" ");
     }
 }
